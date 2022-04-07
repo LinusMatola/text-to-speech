28f662d294b470a2247a5b8032098b91f21bb920
Linus Matola <matolawlinus@gmail.com>
2022-04-07 14:22:54 +0300

    'speech'

---
+++ speech/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+
+namespace speech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SpeechSynthesizer sp = new SpeechSynthesizer();
+                    // Initialize a new instance of the SpeechSynthesizer.  
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+                synth.Volume = 70;
+                synth.Rate = -1;
+                synth.Speak("Ticket number 709, please proceed to counter number 5");
+
+            }
+            
+        }
+    }
+}
